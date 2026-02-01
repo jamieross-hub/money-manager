6f2b67c0f5e2ee8630b444a02e8ca90b93d69128
prashil <prashiln79@gmail.com>
2026-02-01 07:51:22 +0530

    Ui changes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
-  darkMode: 'dark-theme',
+  darkMode: ['class', '.dark-theme'],
   theme: {
     extend: {
       fontFamily: {
